4412960f810665e0c1b83ca2e67e73db968bd910
kyoungd <kyoungd@hotmail.com>
2020-11-22 15:17:05 -0800

    fifth deploy

---
+++ build/precache-manifest.98327230c1db5f645825181481a1dfec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d6c3b0ab75f4a9779b1aeb5180eb11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d49fdba7d6ff81014f69",
+    "url": "/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "bf679e9999cc9066f4d2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d49fdba7d6ff81014f69",
+    "url": "/static/js/2.072e329e.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.072e329e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf679e9999cc9066f4d2",
+    "url": "/static/js/main.e540f309.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
